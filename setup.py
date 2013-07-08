cda96cc7028b9460d348d83fff84d9c0ddb0d9e6
paulgb <paulgb@gmail.com>
2013-07-08 09:38:55 -0400

    bumped version number and pandas requirements

--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@
 from setuptools import setup
 
 setup(name='sklearn-pandas',
-      version='0.0.1',
+      version='0.0.3',
       description='Pandas integration with sklearn',
       author='Paul Butler',
       author_email='paulgb@gmail.com',
       url='https://github.com/paulgb/sklearn-pandas',
       packages=['sklearn_pandas'],
       keywords=['scikit', 'sklearn', 'pandas'],
-      install_requires=['scikit-learn>=0.13.1', 'pandas>=0.10.1']
+      install_requires=[
+          'scikit-learn>=0.13.1',
+          'pandas>=0.11.0',
+          'numpy>=1.6.1']
 )
 
