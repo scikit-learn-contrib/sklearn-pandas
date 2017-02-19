5c0b88d8e70a3749c44beda530429c5f081cb302
gsmafra <gsenamafra@gmail.com>
2017-02-19 12:35:21 -0300

    Add CategoricalImputer working with string columns
    It replaces the null-like values with the mode of the column and works with string-like columns (object dtype in pandas).

--- sklearn_pandas/__init__.py
+++ sklearn_pandas/__init__.py
@@ -2,3 +2,4 @@ __version__ = '1.3.0'
 
 from .dataframe_mapper import DataFrameMapper  # NOQA
 from .cross_validation import cross_val_score, GridSearchCV, RandomizedSearchCV  # NOQA
+from .categorical_imputer import CategoricalImputer
---
+++ tests/test_categorical_imputer.py
@@ -0,0 +1,52 @@
+import pytest
+
+import numpy as np
+import pandas as pd
+
+from sklearn_pandas import CategoricalImputer
+from sklearn_pandas import DataFrameMapper
+
+
+@pytest.mark.parametrize('none_value', [None, np.nan])
+@pytest.mark.parametrize('input_type', ['np', 'pd'])
+def test_unit(input_type, none_value):
+
+    data = ['a', 'b', 'b', none_value]
+
+    if input_type == 'pd':
+        X = pd.Series(data)
+    else:
+        X = np.asarray(data)
+
+    Xc = X.copy()
+
+    Xt = CategoricalImputer().fit_transform(X)
+
+    assert (np.asarray(X) == np.asarray(Xc)).all()
+    assert type(Xt) == np.ndarray
+    assert len(X) == len(Xt)
+    assert len(Xt[pd.isnull(Xt)]) == 0
+
+
+@pytest.mark.parametrize('none_value', [None, np.nan])
+def test_integration(none_value):
+
+    df = pd.DataFrame({'cat': ['a', 'a', 'a', none_value, 'b'],
+                       'num': [1, 2, 3, 4, 5]})
+
+    mapper = DataFrameMapper([
+        ('cat', CategoricalImputer()),
+        ('num', None)
+    ], df_out=True).fit(df)
+
+    df_t = mapper.transform(df)
+
+    assert pd.notnull(df_t).all().all()
+
+    val_idx = pd.notnull(df['cat'])
+    nan_idx = ~val_idx
+
+    assert (df['num'] == df_t['num']).all()
+
+    assert (df['cat'][val_idx] == df_t['cat'][val_idx]).all()
+    assert (df_t['cat'][nan_idx] == df['cat'].mode().values[0]).all()
