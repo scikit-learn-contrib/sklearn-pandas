cb4b4126ca22c9f41223ad5375f3f26dde6ef5e2
gsmafra <gsenamafra@gmail.com>
2017-03-04 16:56:57 -0300

    replacing dataframe imputer by categorical imputer

--- sklearn_pandas/__init__.py
+++ sklearn_pandas/__init__.py
@@ -1,5 +1,5 @@
 __version__ = '1.3.0'
 
 from .dataframe_mapper import DataFrameMapper  # NOQA
-from .dataframe_imputer import DataFrameImputer  # NOQA
 from .cross_validation import cross_val_score, GridSearchCV, RandomizedSearchCV  # NOQA
+from .categorical_imputer import CategoricalImputer
