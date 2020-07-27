67185b1e5f32b4556ce8ee23a15124ff187ef83f
ragrawal <ragrawal@uber.com>
2020-07-26 18:04:32 -0700

    removed functions that are now part of sklearn

--- tests/test_dataframe_mapper.py
+++ tests/test_dataframe_mapper.py
@@ -21,7 +21,8 @@ from sklearn.svm import SVC
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.preprocessing import (
-    Imputer, StandardScaler, OneHotEncoder, LabelBinarizer)
+    StandardScaler, OneHotEncoder, LabelBinarizer)
+from sklearn.impute import SimpleImputer as Imputer
 from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.base import BaseEstimator, TransformerMixin
 import sklearn.decomposition
