a96ae27fd00e4d93e7dd4fec09c24e4ed692a890
Ritesh Agrawal <ragrawal@gmail.com>
2020-07-27 09:31:54 -0700

    Update __init__.py

--- sklearn_pandas/__init__.py
+++ sklearn_pandas/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.8.2'
+__version__ = '2.0.0'
 
 from .dataframe_mapper import DataFrameMapper  # NOQA
 from .cross_validation import cross_val_score, GridSearchCV, RandomizedSearchCV  # NOQA
