b1ee60807cf7fc6c376d6a534332d4f19ba42e26
ragrawal <ragrawal@uber.com>
2020-07-26 23:44:27 -0700

    removed unused imports

--- sklearn_pandas/transformers.py
+++ sklearn_pandas/transformers.py
@@ -1,10 +1,6 @@
 import numpy as np
 import pandas as pd
 
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.utils.validation import check_is_fitted
-
-
 def _get_mask(X, value):
     """
     Compute the boolean mask X == missing_values.
