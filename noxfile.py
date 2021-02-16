0f28b8fba1db50adb628e10d2fda701737667394
Ritesh Agrawal <ragrawal@gmail.com>
2021-02-16 00:05:00 -0800

    Unpin basic libraries

--- noxfile.py
+++ noxfile.py
@@ -2,8 +2,8 @@ import nox
 
 @nox.session
 def lint(session):
-    session.install('pytest==5.3.5', 'setuptools==45.2',
-                    'wheel==0.34.2', 'flake8==3.7.9',
+    session.install('pytest>=5.3.5', 'setuptools>=45.2',
+                    'wheel>=0.34.2', 'flake8>=3.7.9',
                     'numpy==1.18.1', 'pandas==1.0.5')
     session.install('.')
     session.run('flake8', 'sklearn_pandas/', 'tests')
@@ -13,9 +13,9 @@ def lint(session):
 @nox.parametrize('scipy', ['1.4.1', '1.5.4', '1.6.0'])
 @nox.parametrize('pandas', ['1.0.5', '1.1.4', '1.2.2'])
 def tests(session, numpy, scipy, pandas):
-    session.install('pytest==5.3.5', 
-                    'setuptools==45.2',
-                    'wheel==0.34.2',
+    session.install('pytest>=5.3.5', 
+                    'setuptools>=45.2',
+                    'wheel>=0.34.2',
                     f'numpy=={numpy}',
                     f'scipy=={scipy}',
                     f'pandas=={pandas}'
